c21b6ed42acf44f2197fbbede6efced6aaf68262
codekesh <201117@iiitt.ac.in>
2026-04-26 17:56:25 +0530

    initial commit

---
+++ src/main/java/com/aiplatform/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.aiplatform.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
